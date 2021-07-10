52559d234df483917f7084a7df5f1cdcb7c099db
Phạm Duy Phương <zuyfun@gmail.com>
2021-07-10 18:14:45 +0700

    Debug get request body from method POST of route /webhook

--- src/main/java/info/zuyfun/bot/controller/WebhookController.java
+++ src/main/java/info/zuyfun/bot/controller/WebhookController.java
@@ -51,16 +51,16 @@ public class WebhookController {
 		String senderID = null;
 		String messageText = null;
 		if (objJson.getString("object", "").equals("page")) {
-
+			log.info("JsonObject request: {}" + objReq);
+			log.info("JsonObject sender id: {}" + senderID);
+			log.info("JsonObject message text: {}" + messageText);
 			JsonArray objJsonArray = objJson.getJsonArray("entry");
 			for (int i = 0; i < objJsonArray.size(); i++) {
 				JsonObject objMessaging = objJsonArray.getJsonObject(i).getJsonArray("messaging").getJsonObject(0);
 				senderID = objMessaging.getJsonObject("sender").getString("id");
 				messageText = objMessaging.getJsonObject("message").getString("text");
 			}
-			log.info("JsonObject request: {}" + objReq);
-			log.info("JsonObject sender id: {}" + senderID);
-			log.info("JsonObject message text: {}" + messageText);
+
 			return new ResponseEntity<>("EVENT_RECEIVED", HttpStatus.OK);
 		} else {
 			return new ResponseEntity<Object>("NOT_FOUND", HttpStatus.NOT_FOUND);
