0bf94b999b00953e3953e0e05c93df0b088cff0a
Phạm Duy Phương <zuyfun@gmail.com>
2021-07-10 23:13:57 +0700

    Debug get request body from method POST of route /webhook

--- src/main/java/info/zuyfun/bot/service/impl/EventHandlerImpl.java
+++ src/main/java/info/zuyfun/bot/service/impl/EventHandlerImpl.java
@@ -25,37 +25,42 @@ public class EventHandlerImpl implements EventHandler {
 
 	@Override
 	public void handleMessage(String sender_psid, JsonObject received_message) {
-		String jsonStr = "";
-		String messageText = received_message.getString("text", "");
-		JsonArray attachments = received_message.getJsonArray("attachments", null);
-		// Checks if the message contains text
-		if (!messageText.isEmpty()) {
-			// Create the payload for a basic text message, which
-			// will be added to the body of our request to the Send API
-			String messageResponse = "";
-			if (!callSimsimi(messageText).isEmpty()) {
-				JsonObject simsimiResponse = new JsonObject(callSimsimi(messageText));
-				messageResponse = simsimiResponse.getString("success");
+		try {
+			String jsonStr = "";
+			String messageText = received_message.getString("text", "");
+			JsonArray attachments = received_message.getJsonArray("attachments", null);
+			// Checks if the message contains text
+			if (!messageText.isEmpty()) {
+				// Create the payload for a basic text message, which
+				// will be added to the body of our request to the Send API
+				String messageResponse = "";
+				JsonObject json = new JsonObject(callSimsimi(messageText));
+				if (json != null) {
+					messageResponse = json.getString("success");
+				}
+
+				jsonStr = "{\r\n     \"text\": \"" + messageResponse + "\"    }";
+			} else if (attachments != null) {
+				// Get the URL of the message attachment
+				String attachmentUrl = attachments.getJsonObject(0).getJsonObject("payload").getString("url");
+				jsonStr = "{\r\n" + "      \"attachment\": {\r\n" + "        \"type\": \"template\",\r\n"
+						+ "        \"payload\": {\r\n" + "          \"template_type\": \"generic\",\r\n"
+						+ "          \"elements\": [{\r\n"
+						+ "            \"title\": \"Is this the right picture?\",\r\n"
+						+ "            \"subtitle\": \"Tap a button to answer.\",\r\n" + "            \"image_url\": "
+						+ attachmentUrl + ",\r\n" + "            \"buttons\": [\r\n" + "              {\r\n"
+						+ "                \"type\": \"postback\",\r\n" + "                \"title\": \"Yes!\",\r\n"
+						+ "                \"payload\": \"yes\",\r\n" + "              },\r\n" + "              {\r\n"
+						+ "                \"type\": \"postback\",\r\n" + "                \"title\": \"No!\",\r\n"
+						+ "                \"payload\": \"no\",\r\n" + "              }\r\n" + "            ],\r\n"
+						+ "          }]\r\n" + "        }\r\n" + "      }\r\n" + "    }\r\n" + "  } ";
+				// Send the response message
+
 			}
-
-			jsonStr = "{\r\n     \"text\": \"" + messageResponse + "\"    }";
-		} else if (attachments != null) {
-			// Get the URL of the message attachment
-			String attachmentUrl = attachments.getJsonObject(0).getJsonObject("payload").getString("url");
-			jsonStr = "{\r\n" + "      \"attachment\": {\r\n" + "        \"type\": \"template\",\r\n"
-					+ "        \"payload\": {\r\n" + "          \"template_type\": \"generic\",\r\n"
-					+ "          \"elements\": [{\r\n" + "            \"title\": \"Is this the right picture?\",\r\n"
-					+ "            \"subtitle\": \"Tap a button to answer.\",\r\n" + "            \"image_url\": "
-					+ attachmentUrl + ",\r\n" + "            \"buttons\": [\r\n" + "              {\r\n"
-					+ "                \"type\": \"postback\",\r\n" + "                \"title\": \"Yes!\",\r\n"
-					+ "                \"payload\": \"yes\",\r\n" + "              },\r\n" + "              {\r\n"
-					+ "                \"type\": \"postback\",\r\n" + "                \"title\": \"No!\",\r\n"
-					+ "                \"payload\": \"no\",\r\n" + "              }\r\n" + "            ],\r\n"
-					+ "          }]\r\n" + "        }\r\n" + "      }\r\n" + "    }\r\n" + "  } ";
-			// Send the response message
-
+			callSendAPI(sender_psid, jsonStr);
+		} catch (Exception e) {
+			log.error("*** handleMessage System error: " + e);
 		}
-		callSendAPI(sender_psid, jsonStr);
 	}
 
 	@Override
@@ -93,9 +98,9 @@ public class EventHandlerImpl implements EventHandler {
 			clientPool = WebClient.create(SIMSIMI_URL);
 			clientPool.query("text", messageText).query("lang", "vi_VN");
 			Response response = clientPool.get();
-				result = response.readEntity(String.class);
-				log.info("*** Data response " + result);
-		
+			result = response.readEntity(String.class);
+			log.info("*** Data response " + result);
+
 		} catch (Exception ex) {
 			log.error("*** Lỗi hệ thống - Exception: " + ex);
 		}
