d90451376c57b58ba2dc752fae7c793b2b1e6193
Phạm Duy Phương <zuyfun@gmail.com>
2021-07-11 12:45:19 +0700

    Update performance of request

--- src/main/java/info/zuyfun/bot/service/impl/EventHandlerImpl.java
+++ src/main/java/info/zuyfun/bot/service/impl/EventHandlerImpl.java
@@ -13,6 +13,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -117,7 +118,8 @@ public class EventHandlerImpl implements EventHandler {
 		// Construct the message body
 		// Send the HTTP request to the Messenger Platform
 		try {
-			restTemplate.postForObject(fbURLSender, objRequest, Request.class);
+			HttpEntity<String> request = new HttpEntity<String>(mapper.writeValueAsString(objRequest));
+			restTemplate.postForObject(fbURLSender, request, Request.class);
 			logger.info("Response Object {}", clientPool.getResponse());
 		} catch (Exception e) {
 			logger.error("*** callSendAPI System Error: " + e);
