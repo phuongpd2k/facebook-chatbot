272770b379017444e6a6c1b47bf33cf7eb46e54a
Phạm Duy Phương <zuyfun@gmail.com>
2021-07-12 10:13:11 +0700

    ADD Class Config to project

--- src/main/java/info/zuyfun/bot/BotApplication.java
+++ src/main/java/info/zuyfun/bot/BotApplication.java
@@ -3,18 +3,12 @@ package info.zuyfun.bot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 @ComponentScan(basePackages = "info.zuyfun.bot")
 public class BotApplication {
-	@Bean
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
 	public static void main(String[] args) {
 		SpringApplication.run(BotApplication.class, args);
 	}
---
+++ src/main/java/info/zuyfun/bot/config/BotConfig.java
@@ -0,0 +1,15 @@
+package info.zuyfun.bot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestTemplate;
+
+@EnableScheduling
+@Configuration
+public class BotConfig {
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+}
