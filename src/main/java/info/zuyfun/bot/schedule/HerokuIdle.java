4cbd0de8c50246e82fb68ee133be9d4e3da8609f
Phạm Duy Phương <zuyfun@gmail.com>
2021-07-12 19:14:21 +0700

    DEBUG Action typing_on

--- src/main/java/info/zuyfun/bot/schedule/HerokuIdle.java
+++ src/main/java/info/zuyfun/bot/schedule/HerokuIdle.java
@@ -9,14 +9,12 @@ import org.springframework.web.client.RestTemplate;
 public class HerokuIdle {
 	private static final Logger logger = LoggerFactory.getLogger(HerokuIdle.class);
 
-	@Autowired
-	RestTemplate restTemplate;
 
 	@Scheduled(cron = "0 0/10 * * * ?")
 	public void herokuIdle() {
 		try {
-			String result = restTemplate.getForObject("https://info-zuy-bot.herokuapp.com/herokuIdle", String.class);
-			logger.info("***herokuIdle Execution: {}", result);
+//			String result = restTemplate.getForObject("https://info-zuy-bot.herokuapp.com/herokuIdle", String.class);
+//			logger.info("***herokuIdle Execution: {}", result);
 		} catch (Exception e) {
 			logger.error("***herokuIdle Exception: {}", e);
 		}
