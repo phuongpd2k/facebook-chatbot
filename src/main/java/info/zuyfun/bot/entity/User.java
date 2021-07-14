87679f5b04be663383f7007b0b314cc6e9a32fbd
Phạm Duy Phương <zuyfun@gmail.com>
2021-07-14 14:24:36 +0700

    DEBUG MYSQL Connection

--- src/main/java/info/zuyfun/bot/entity/User.java
+++ src/main/java/info/zuyfun/bot/entity/User.java
@@ -11,7 +11,7 @@ import javax.persistence.Table;
 import lombok.Data;
 
 @Entity
-@Table(name = "user")
+@Table(name = "USER", schema = "public")
 @Data
 public class User implements Serializable {
 	/**
