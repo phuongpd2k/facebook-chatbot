222c40341b863d7ef3fc59f025aae2039debf860
Phạm Duy Phương <zuyfun@gmail.com>
2021-07-17 11:52:40 +0700

    DEBUG Response Username

--- src/main/java/info/zuyfun/bot/constants/CommandConstants.java
+++ src/main/java/info/zuyfun/bot/constants/CommandConstants.java
@@ -1,8 +1,8 @@
 package info.zuyfun.bot.constants;
 
 public class CommandConstants {
-	public static final String OFF = "on";
-	public static final String ON = "off";
+	public static final String OFF = "off";
+	public static final String ON = "on";
 	public static final String CHAT_WITH_BOT = "/chat";
 	public static final String NOTIFICATION = "/notify";
 	public static final String KQXS = "kqxs";
