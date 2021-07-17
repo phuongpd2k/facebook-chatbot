1ebe2339a00f23b319da28d4307477e7e8c7898c
Phạm Duy Phương <zuyfun@gmail.com>
2021-07-17 11:34:30 +0700

    DEBUG Response Username

--- src/main/java/info/zuyfun/bot/facebook/service/impl/MessageServiceImpl.java
+++ src/main/java/info/zuyfun/bot/facebook/service/impl/MessageServiceImpl.java
@@ -176,7 +176,7 @@ public class MessageServiceImpl implements MessageService {
 
 	public void patternCommand(BigDecimal senderID, String messageText) {
 		String[] textArray = messageText.split("//s+");
-		logger.info("***textArray : {}", textArray);
+		logger.info("***textArray : {}", textArray.length);
 		Request objRequest = null;
 		try {
 			if (textArray.length == 1) {
