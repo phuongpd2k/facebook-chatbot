96dde4be36f066d77f1e3e6dd5bf13389fc1ca8b
Phạm Duy Phương <zuyfun@gmail.com>
2021-07-19 15:11:20 +0700

    UPDATE message

--- src/main/java/info/zuyfun/bot/constants/MessageConstants.java
+++ src/main/java/info/zuyfun/bot/constants/MessageConstants.java
@@ -8,7 +8,7 @@ public class MessageConstants {
 	public static final String MESSAGE_ERROR = "Xin lỗi, tôi không hiểu câu lệnh của bạn! Hãy gõ \"/help\" để xem các câu lệnh!";
 	public static final String TURN_ON_CHAT_BOT = "Bạn đã bật tính năng nói chuyện với bot ngu thành công 🙆";
 	public static final String TURN_OFF_CHAT_BOT = "Bạn đã tắt tính năng nói chuyện với bot ngu thành công 🙋";
-	public static final String OWNER_INFO = "Phạm Duy Phương là chủ của tôi <3";
+	public static final String OWNER_INFO = "Tại sao lại nhắc đến tên cậu ấy, ngươi muốn gì từ chủ của ta ???";
 	public static final String HELP = "Đây là các lệnh mà tôi hỗ trợ:\n\n"
 			+ "+ /chat [on/off] : Bạn sẽ có thể bật hoặc tắt chức năng trò chuyện trực tiếp với tôi!\n";
 
